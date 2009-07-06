jwendell/gtk-vnc

commit 1e121b4084788399c4d71d60b513997661e3116e

I make the patch that applied your two indications. Moreover, this patch contains the fix of two bugs and a modification besides the indications.
The change points from the last patch:

1)Use GDK key symbols instead of X11 key symbols.

2)Remove CR characters.

3)Make keycode conversion function.
  - I make the keycode conversion function on "x_keymap.c".

4)Modify structure used for keycode conversion.
  - The type of the keycodes member variable is changed from "guint*"
    to "GdkKeymapKey*".

5)Modify getting timing and release timing of keymap entries.
  - Because "g_free" function is not called on the last patch, the memory leak is done.

src/vncdisplay.c
@@ -568,6 +568,8 @@ static gboolean key_event(GtkWidget *widget, GdkEventKey *key,
 					    &level,
 					    &consumed);
 
+	keyval = x_keymap_get_keyval_from_keycode(key->hardware_keycode, keyval);
+
 	/*
 	 * More VNC suckiness with key state & modifiers in particular
 	 *
@@ -1357,6 +1359,8 @@ static void *vnc_coroutine(void *opaque)
 	}
 
 	GVNC_DEBUG("Started background coroutine\n");
+	x_keymap_set_keymap_entries();
+
 	if (priv->fd != -1) {
 		if (!gvnc_open_fd(priv->gvnc, priv->fd))
 			goto cleanup;
@@ -1404,6 +1408,7 @@ static void *vnc_coroutine(void *opaque)
 	gvnc_close(priv->gvnc);
 	emit_signal_delayed(obj, VNC_DISCONNECTED, &s);
 	g_idle_add(delayed_unref_object, obj);
+	x_keymap_free_keymap_entries();
 	/* Co-routine exits now - the VncDisplay object may no longer exist,
 	   so don't do anything else now unless you like SEGVs */
 	return NULL;

src/x_keymap.c
@@ -33,6 +33,7 @@
  */
 
 #include "x_keymap.h"
+#include <gdk/gdkkeysyms.h>
 
 static const uint8_t x_keycode_to_pc_keycode_table[115] = {
    0xc7,      /*  97  Home   */
@@ -116,6 +117,17 @@ static const uint8_t x_keycode_to_pc_keycode_table[115] = {
    0x73,         /* 211 backslash */
 };
 
+/* keycode translation for sending ISO_Left_Send
+ * to vncserver
+ */
+static struct {
+	GdkKeymapKey *keys;
+	gint n_keys;
+	guint keyval;
+} untranslated_keys[] = {{NULL, 0, GDK_Tab}};
+
+static unsigned int ref_count_for_untranslated_keys = 0;
+
 /* FIXME N.B. on Windows, gtk probably returns PC scan codes */
 
 uint8_t x_keycode_to_pc_keycode(int keycode)
@@ -130,4 +142,45 @@ uint8_t x_keycode_to_pc_keycode(int keycode)
 		keycode = 0;
 
 	return keycode;
+}
+
+/* Set the keymap entries */
+void x_keymap_set_keymap_entries()
+{
+	int i;
+	if (ref_count_for_untranslated_keys == 0)
+		for (i = 0; i < sizeof(untranslated_keys) / sizeof(untranslated_keys[0]); i++)
+			gdk_keymap_get_entries_for_keyval(gdk_keymap_get_default(),
+							  untranslated_keys[i].keyval,
+							  &untranslated_keys[i].keys,
+							  &untranslated_keys[i].n_keys);
+	ref_count_for_untranslated_keys++;
+}
+
+/* Free the keymap entries */
+void x_keymap_free_keymap_entries()
+{
+	int i;
+
+	if (ref_count_for_untranslated_keys == 0)
+		return;
+
+	ref_count_for_untranslated_keys--;
+	if (ref_count_for_untranslated_keys == 0)
+		for (i = 0; i < sizeof(untranslated_keys) / sizeof(untranslated_keys[0]); i++)
+			g_free(untranslated_keys[i].keys);
+
+}
+
+/* Get the keyval from the keycode without the level. */
+guint x_keymap_get_keyval_from_keycode(guint keycode, guint keyval)
+{
+	int i, k;
+	for (i = 0; i < sizeof(untranslated_keys) / sizeof(untranslated_keys[0]); i++) {
+		if (keycode == untranslated_keys[i].keys[0].keycode) {
+			return untranslated_keys[i].keyval;
+		}
+	}
+
+	return keyval;
 }

src/x_keymap.h
@@ -2,7 +2,11 @@
 #define _GTK_VNC_X_KEYMAP_H
 
 #include <stdint.h>
+#include <gdk/gdk.h>
 
 uint8_t x_keycode_to_pc_keycode(int keycode);
+void x_keymap_set_keymap_entries();
+void x_keymap_free_keymap_entries();
+guint x_keymap_get_keyval_from_keycode(guint keycode, guint keyval);
 
 #endif

commit b0a5d4d92884c4380bfbd23de6520ddbf41d91ca

Re-fix Shift+Tab keyval translation from 1e121b4084788399c4d71d60b513997661e3116e, accidentally reverted in 4f4103fbc7021a3ae8e9c43e1f561cd8f65481aa

src/vncdisplay.c
@@ -678,6 +678,7 @@ static gboolean key_event(GtkWidget *widget, GdkEventKey *key)
 {
 	VncDisplayPrivate *priv = VNC_DISPLAY(widget)->priv;
 	int i;
+	int keyval = key->keyval;
 
 	if (priv->gvnc == NULL || !gvnc_is_initialized(priv->gvnc))
 		return FALSE;
@@ -687,7 +688,9 @@ static gboolean key_event(GtkWidget *widget, GdkEventKey *key)
 
 	GVNC_DEBUG("%s keycode: %d  state: %d  group %d, keyval: %d",
 		   key->type == GDK_KEY_PRESS ? "press" : "release",
-		   key->hardware_keycode, key->state, key->group, key->keyval);
+		   key->hardware_keycode, key->state, key->group, keyval);
+
+	keyval = x_keymap_get_keyval_from_keycode(key->hardware_keycode, keyval);
 
 	/*
 	 * Some VNC suckiness with key state & modifiers in particular
@@ -737,18 +740,18 @@ static gboolean key_event(GtkWidget *widget, GdkEventKey *key)
 	if (key->type == GDK_KEY_PRESS) {
 		for (i = 0 ; i < (int)(sizeof(priv->down_keyval)/sizeof(priv->down_keyval[0])) ; i++) {
 			if (priv->down_scancode[i] == 0) {
-				priv->down_keyval[i] = key->keyval;
+				priv->down_keyval[i] = keyval;
 				priv->down_scancode[i] = key->hardware_keycode;
 				/* Send the actual key event we're dealing with */
-				gvnc_key_event(priv->gvnc, 1, key->keyval, key->hardware_keycode);
+				gvnc_key_event(priv->gvnc, 1, keyval, key->hardware_keycode);
 				break;
 			}
 		}
 	}
 
 	if (key->type == GDK_KEY_PRESS &&
-	    ((key->keyval == GDK_Control_L && (key->state & GDK_MOD1_MASK)) ||
-	     (key->keyval == GDK_Alt_L && (key->state & GDK_CONTROL_MASK)))) {
+	    ((keyval == GDK_Control_L && (key->state & GDK_MOD1_MASK)) ||
+	     (keyval == GDK_Alt_L && (key->state & GDK_CONTROL_MASK)))) {
 		if (priv->in_pointer_grab)
 			do_pointer_ungrab(VNC_DISPLAY(widget), FALSE);
 		else if (!priv->grab_keyboard || !priv->absolute)