965b695957824bfaacb77a5a0a4de0a37e24b849
Daniel P. Berrange <berrange@redhat.com>
2010-03-21 14:09:09 +0000

    Turn on debug in javascript example program
    * src/Makefile.am: Include vncutil.h/.c in introspection processing
    * examples/gvncviewer.js: Enable debugging

--- examples/gvncviewer.js
+++ examples/gvncviewer.js
@@ -1,9 +1,12 @@
 #!/usr/bin/gjs
 
 const Vnc = imports.gi.GtkVnc;
+const GVnc = imports.gi.GVnc;
 const Gtk = imports.gi.Gtk;
 
 Gtk.init(0, null);
+
+GVnc.util_set_debug(true);
 
 var win = new Gtk.Window({title: "GTK-VNC with JavaScript"});
 var disp = new Vnc.Display();
